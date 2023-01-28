ce3a37d3ad7cd1ac5fcc22c16db892058beafe3f
Felipe Leal <felipewanderson2020@gmail.com>
2023-01-28 00:51:43 -0300

    feat: estrutura inicial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/defaults";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <h1>Hello World</h1>
+    </ThemeProvider>
+  )
+}
+
